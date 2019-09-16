3f4a70246e39c94445001573bc9ceb2b7ca4b086
kwhjvdkamp <info@van-der-kamp.nl>
2019-09-16 12:44:32 +0200

    modifications

--- firstpackage/tests/test_documentmedia.py
+++ firstpackage/tests/test_documentmedia.py
@@ -2,20 +2,26 @@ from collections import Counter
 
 from firstpackage.src.documentmedia import DocumentMedia as doc_med
 
-# Tests on Document object
-# -------------------------------------------------------------------
-# blank document
-def test_document_empty():
-    doc = doc_med('')
-    assert doc.tokens == []
-    assert doc.word_counts == Counter({})
+# # Tests on Document object
+# # -------------------------------------------------------------------
+# # blank document
+# def test_document_empty():
+#     doc = doc_med('')
+#     assert doc.tokens != []  # Test Pass
+#       assert doc.tokens == []  # Test Fails, because
+#       # checking an empty document delivers a generator object (doc.tokens) 
+#       # which itself apparently is an empty 'box' (if I may say so) 
+#       # which is not equal to an empyt list
+#       # Whats the purpose of this test?
+#     assert doc.word_counts == Counter({}) # Test Pass, 
+#       # seems obvious, no word counted compared to the Counter-object which is empty
 
 
-# # ------------------------------------------------------------------- 
-# # attribute
-# def test_document_tokens():
-#     doc = doc_med('a e i o u')
-#     assert doc.tokens == ['a', 'e', 'i', 'o', 'u']
+# ------------------------------------------------------------------- 
+# attribute
+def test_document_tokens():
+    doc = doc_med('a e i o u')
+    assert doc.tokens == ['a', 'e', 'i', 'o', 'u']   # I expect this assert as True >> pytest -v says False ???
 
   
 # # ------------------------------------------------------------------- 
