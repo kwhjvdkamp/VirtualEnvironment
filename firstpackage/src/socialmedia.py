7ee571a3dd0e44eb8154da94ec19d60704dd5628
kwhjvdkamp <info@van-der-kamp.nl>
2020-08-05 23:05:09 +0200

    fixes

--- firstpackage/src/socialmedia.py
+++ firstpackage/src/socialmedia.py
@@ -8,10 +8,10 @@ from .documentmedia import DocumentMedia
 # private function
 def filter_word_counts(word_counts, first_char):
     """Filter Document.word_counts by the first character of the word
-    
+
     :param word_counts: the word_counts attribute of a Document class instance
     :param first_char: only keep word counts that start with this character
-    
+
     >>> # How to filter to only words that start with 'A'
     >>> filter_word_counts(document.word_counts, 'A')
     """
@@ -22,7 +22,7 @@ def filter_word_counts(word_counts, first_char):
 # inherited class
 class SocialMedia(DocumentMedia):
     """Analyze text data from social media
-    
+
     :param text: social media text to analyze
 
     :ivar hashtag_counts: Counter object containing counts of hashtags used in text
@@ -30,14 +30,14 @@ class SocialMedia(DocumentMedia):
     """
     def __init__(self, text):
         DocumentMedia.__init__(self, text)
-        
+
         self.hashtag_counts = self._count_hashtags()
         self.mention_counts = self._count_mentions()
 
     def _count_hashtags(self):
         # Filter attribute so only words starting with '#' remain
-        return filter_word_counts(self.word_counts, first_char='#')      
-    
+        return filter_word_counts(self.word_counts, first_char='#')
+
     def _count_mentions(self):
         # Filter attribute so only words starting with '@' remain
         return filter_word_counts(self.word_counts, first_char='@')
